f5b5029397dfa86b38fdf4298667fd5c5b99889b
andres <andres.vanegas@gmail.com>
2026-05-25 15:19:52 -0500

    se agrega una pagina para dashboard

---
+++ src/page/dashboard-page.tsx
@@ -0,0 +1,25 @@
+import { TargetDashboardComponent } from "../components/target-dashboard-component";
+import { dashboardActions } from "../helper/dashboardsActions";
+
+export const DashboardPage = () => {
+  return (
+    <>
+      <header className="bg-gray-800 text-white p-4 flex justify-between items-center">
+        <h1>Manejador de restaurantes</h1>
+        <button className="bg-red-600 p-2 rounded-lg">salir</button>
+      </header>
+      <main className="p-6 max-w-7xl mx-auto">
+        <h2 className="text-3xl font-bold">Hi, Andres 👋🏽</h2>
+        <h2 className="text-xl font-bold">Administrador - medellin</h2>
+        <hr className="my-2" />
+        <section className="flex gap-5 justify-center flex-wrap">
+          <h2 className="w-full text-xl font-bold">what will you do?</h2>
+
+          { dashboardActions.map((action) => (
+            <TargetDashboardComponent imagen={action.image} title={action.title} url={action.url} />
+          ))}
+        </section>
+      </main>
+    </>
+  );
+};
