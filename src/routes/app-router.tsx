801abc2d86c4a00319c8faca9024aea340ef0248
andres <andres.vanegas@gmail.com>
2026-05-25 18:43:49 -0500

    actualizacion de la pagina de registro

--- src/routes/app-router.tsx
+++ src/routes/app-router.tsx
@@ -1,9 +1,29 @@
 import { createBrowserRouter } from "react-router-dom";
 import { HomePage } from "../page/home-page";
+import { LoginPage } from "../page/login-page";
+import { RegisterPage } from "../page/register-page";
+// import { DashboardPage } from "../page/dashboard-page";
+import { CreateRestaurantPage } from "../page/create-restaurant-page";
 
 export const routes = createBrowserRouter([
     {
-       path:"/home",
+       path:"",
        element: <HomePage/> 
+    },
+    {
+        path: "/login",
+        element: <LoginPage/>
+    },
+    {
+        path: "/register",
+        element: <RegisterPage/>
+    },
+    // {
+    //     path: "/private/dashboard",
+    //     element: <DashboardPage/>
+    // },
+    {
+        path: "/private/create-restaurant",
+        element: <CreateRestaurantPage/>
     }
 ])
