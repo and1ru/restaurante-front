f8c5af0b80edd8eaa7afe288af1538a58a87cf71
andres <andres.vanegas@gmail.com>
2026-05-27 07:38:48 -0500

    se agrego una pagina para el manejo de empleados

---
+++ src/components/employee-card-component.tsx
@@ -0,0 +1,10 @@
+export const EmployeeCardComponent = () => {
+  return (
+    <div className="shadow-xl w-full p-4 rounded-lg">
+        <p>Andre Felipe</p>
+        <p>Administrador - medellín</p>
+        <button className="bg-red-500 p-2 rounded-lg font-bold text-white">depedir</button>
+        <button className="bg-blue-500 p-2 rounded-lg font-bold text-white">cambiar rol</button>
+    </div>
+  );
+};
--- src/routes/app-router.tsx
+++ src/routes/app-router.tsx
@@ -4,6 +4,7 @@ import { LoginPage } from "../page/login-page";
 import { RegisterPage } from "../page/register-page";
 import { DashboardPage } from "../page/dashboard-page";
 import { CreateRestaurantPage } from "../page/create-restaurant-page";
+import { ManageEmployeePage } from "../page/manage-employee-page";
 
 export const routes = createBrowserRouter([
     {
@@ -25,5 +26,9 @@ export const routes = createBrowserRouter([
     {
         path: "/private/create-restaurant",
         element: <CreateRestaurantPage/>
+    },
+    {
+        path: "/private/manage-employee",
+        element: <ManageEmployeePage/>
     }
 ])
